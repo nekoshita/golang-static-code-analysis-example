ffd2be157fd545a5d7f51bdf5acde5b15a92de15
Nekoshita Yuki <yuki.303442@gmail.com>
2021-03-21 23:31:13 +0900

    feat: fail ci on purpose

--- main.go
+++ main.go
@@ -3,7 +3,19 @@ package main
 import "log"
 
 func main() {
-	
-	
+
 	log.Print("hello")
+
+	if _, err := foo(); err != nil {
+		panic(err)
+	}
+
+}
+
+type HogeYey struct {
+	Foo string
+}
+
+func foo() (string, error) {
+	return "", nil
 }
